f8058751c67ede6853d6449d6c88587f896a382a
Peter Makowski <peter@makowski.uk>
2020-06-11 16:33:52 +0100

    add hand tracking toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import StepSequencer from "./StepSequencer";
 import HandTrack from "./HandTrack";
@@ -6,15 +6,20 @@ import HandTrack from "./HandTrack";
 import Pickup from "./components/Pickup";
 
 function App() {
+  const [isHandTrackingEnabled, setIsHandTrackingEnabled] = useState(false);
   return (
     <div className="App">
-      <HandTrack
-        onPositionChange={(position) =>
-          console.log("Detected position: ", position)
-        }
-      />
+      <button onClick={() => setIsHandTrackingEnabled(!isHandTrackingEnabled)}>
+        {isHandTrackingEnabled ? "disable" : "enable"} hand tracking
+      </button>
+      {isHandTrackingEnabled && (
+        <HandTrack
+          onPositionChange={(position) =>
+            console.log("Detected position: ", position)
+          }
+        />
+      )}
       <StepSequencer />
-      <header className="App-header"></header>
       {/* <Pickup /> */}
     </div>
   );
